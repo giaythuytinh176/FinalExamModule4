8dfddb9fa5cc15f9e5c49af3eebcaa9a7fa004e3
LE DUC TAM <giaythuytinh176@gmail.com>
2021-01-28 02:35:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FinalExampleModule4
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './component/sidenav/sidenav.component';
+import { TopnavComponent } from './component/topnav/topnav.component';
+import { FooterComponent } from './component/footer/footer.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent,
+    TopnavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
