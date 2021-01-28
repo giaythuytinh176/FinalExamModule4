d0c127b56fc1aac5ea01ff03a471fd5bd88b5f14
LE DUC TAM <giaythuytinh176@gmail.com>
2021-01-28 11:13:27 +0700

    upload

--- frontend/src/app/component/book-list/book-list.component.ts
+++ frontend/src/app/component/book-list/book-list.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Book} from '../../service/book';
 import {Observable} from 'rxjs';
 import {ToastrService} from 'ngx-toastr';
 import {Router} from '@angular/router';
 import {BookService} from '../../service/book.service';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-book-list',
@@ -17,6 +18,7 @@ export class BookListComponent implements OnInit {
   constructor(private bookService: BookService,
               private router: Router,
               private toasrt: ToastrService,
+              public dialog: MatDialog,
   ) {
   }
 
@@ -30,7 +32,7 @@ export class BookListComponent implements OnInit {
   }
 
   deleteBook(id: number): void {
-    if (confirm('Do you want to delete it?')) {
+    // if (confirm('Do you want to delete it?')) {
       this.bookService.deleteBook(id)
         .subscribe(
           data => {
@@ -42,11 +44,51 @@ export class BookListComponent implements OnInit {
             console.log(error);
             this.toasrt.warning('Có lỗi xảy ra, không thể xoá được file.', 'Error happing while deleting!');
           });
-    }
+    // }
   }
 
   bookDetails(id: number): void {
     this.router.navigate(['details', id]);
   }
 
+  // tslint:disable-next-line:typedef
+  openDialog(id: number, title: string, author: string, description: string): void {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog, {
+      width: '250px',
+      data: {name: id, title: title, author: author, description: description}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.title = result;
+      if (result) {
+        this.deleteBook(id);
+      }
+      console.log(result);
+    });
+  }
+}
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'dialog-content-example-dialog.html',
+})
+// tslint:disable-next-line:component-class-suffix
+export class DialogContentExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogContentExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+
+export interface DialogData {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
 }
